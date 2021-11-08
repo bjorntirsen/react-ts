1692e19adf4df351cde63064945059439e48bce2
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-08 13:47:33 +0100

    :sparkles: Functional Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 
 const App: FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos, setTodos] = useState([{ id: 't1', text: 'Finish the course' }]);
+  const newTodoHandler = (todoText: string) => {
+    const id = `t${todos.length + 1}`;
+    setTodos((oldTodos) => [...oldTodos, { id, text: todoText }]);
+  };
   return (
     <div className='App'>
-      <NewTodo />
+      <NewTodo handleNewTodo={newTodoHandler} />
       <TodoList items={todos} />
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,13 +1,29 @@
-import { FC } from 'react';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { FC, FormEvent, useRef } from 'react';
 
-const NewTodo: FC = () => (
-  <form>
-    <div>
-      <label htmlFor='todo-text'>Todo Text</label>
-      <input type='text' id='todo-text' />
-    </div>
-    <button type='submit'>ADD TODO</button>
-  </form>
-);
+interface Props {
+  handleNewTodo: (a: string) => void;
+}
+
+const NewTodo: FC<Props> = ({ handleNewTodo }) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const todoSubmitHandler = (event: FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    handleNewTodo(enteredText);
+    textInputRef.current!.value = '';
+  };
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label htmlFor='todo-text'>Todo Text</label>
+        <input type='text' id='todo-text' ref={textInputRef} />
+      </div>
+      <button type='submit'>ADD TODO</button>
+    </form>
+  );
+};
 
 export default NewTodo;
